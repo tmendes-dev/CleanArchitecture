0d4ad52752e4dcdbbde4bf1822b2aba77f25de8a
Thomas Mendes <th.mendes1997@gmail.com>
2021-12-08 16:40:55 -0300

    Repositories Interfaces

---
+++ CleanArchitecture.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetByIdAsync(int? id);
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+
+    }
+}
---
+++ CleanArchitecture.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
