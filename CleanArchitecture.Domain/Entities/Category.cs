bbc0726aa21f4ecc690e2d59908f6c63a80648da
Thomas Mendes <th.mendes1997@gmail.com>
2021-12-08 16:28:30 -0300

    Project References, Domain implementation

---
+++ CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitecture.Domain.Validation;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
+            Name = name;
+        }
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+            Name = name;
+        }
+    }
+}
